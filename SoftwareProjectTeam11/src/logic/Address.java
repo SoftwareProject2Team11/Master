894f5d5f88f3ed5dab5697ce436cb5ba1eca887b
oussamaelaazmani <oussama.el.aazmani@student.ehb.be>
2017-10-26 11:00:11 +0200

    Project aangemaakt
    - Project
    - logic
    - klasse Address

---
+++ SoftwareProjectTeam11/src/logic/Address.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Address {
+
+	private String street;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+}
