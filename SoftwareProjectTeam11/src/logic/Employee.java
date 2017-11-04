001760612e7088034f052d88ffa22ffb02a9a800
Basil LK <32955738+Basil-LK@users.noreply.github.com>
2017-11-04 15:25:24 +0100

    Create Employee and Certificate
    creation of basicclasses

---
+++ SoftwareProjectTeam11/src/logic/Employee.java
@@ -0,0 +1,33 @@
+package logic;
+
+//import java.util.List;
+//import java.util.ArrayList;
+
+public class Employee {
+
+	private int employeeId;
+	private boolean isActief;
+	
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	public boolean isActief() {
+		return isActief;
+	}
+	public void setActief(boolean isActief) {
+		this.isActief = isActief;
+	}
+	
+	/*
+	List <Certificate> list = new ArrayList<Certificate>();
+	List <Material> list = new ArrayList<Material>();
+	List <Survey> list = new ArrayList<Survey>();
+	*/
+	
+	
+	
+}
