cb8a2c458aa737ab785b1a17bd6a98839e18ec9b
Basil LK <32955738+Basil-LK@users.noreply.github.com>
2017-11-05 17:44:16 +0100

    Adding Hibernate
    Toevoeging van Hibernate Jar files + connector mySQL + XML voor het maken van DAO's

--- SoftwareProjectTeam11/src/logic/Address.java
+++ SoftwareProjectTeam11/src/logic/Address.java
@@ -2,13 +2,45 @@ package logic;
 
 public class Address {
 
-	private String street;
-
-	public String getStreet() {
-		return street;
-	}
-
-	public void setStreet(String street) {
-		this.street = street;
-	}
+    private int addressId;
+    private String streetname;
+    private int houseNumber;
+    private String city;
+    public Address(String streetname, int houseNumber, String city) {
+        super();
+        this.streetname = streetname;
+        this.houseNumber = houseNumber;
+        this.city = city;
+    }
+    public Address(int addressId,String streetname, int houseNumber, String city) {
+        super();
+        this.addressId = addressId;
+        this.streetname = streetname;
+        this.houseNumber = houseNumber;
+        this.city = city;
+    }
+    public String getStreetname() {
+        return streetname;
+    }
+    public void setStreetname(String streetname) {
+        this.streetname = streetname;
+    }
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public int getAddressId() {
+        return addressId;
+    }
+    public void setAddressId(int addressId) {
+        this.addressId = addressId;
+    }
 }
