07dad436bb99dd02fac6d2ebade78c8b804c56f4
oussamaelaazmani <oussama.el.aazmani@student.ehb.be>
2017-11-05 16:53:02 +0100

    Klasses aanmaken
    Klassen Answer,Question,Survey

---
+++ SoftwareProjectTeam11/src/logic/Question.java
@@ -0,0 +1,46 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Question {
+	
+	private int questionId;
+	private int surveyId;
+	private ArrayList<String> questions;
+	
+	public Question(int questionId, int surveyId, ArrayList<String> questions) {
+		super();
+		this.questionId = questionId;
+		this.surveyId = surveyId;
+		this.questions = questions;
+	}
+
+	public int getQuestionId() {
+		return questionId;
+	}
+
+	public void setQuestionId(int questionId) {
+		this.questionId = questionId;
+	}
+
+	public int getSurveyId() {
+		return surveyId;
+	}
+
+	public void setSurveyId(int surveyId) {
+		this.surveyId = surveyId;
+	}
+
+	public ArrayList<String> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(ArrayList<String> questions) {
+		this.questions = questions;
+	}
+	
+	
+	
+	
+
+}
