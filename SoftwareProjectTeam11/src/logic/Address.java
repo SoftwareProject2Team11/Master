c70a3b42968a072af50d35c531f58cd19adc5e93
oussamaelaazmani <oussama.el.aazmani@student.ehb.be>
2017-11-05 14:16:59 +0100

    address

--- SoftwareProjectTeam11/src/logic/Address.java
+++ SoftwareProjectTeam11/src/logic/Address.java
@@ -2,13 +2,33 @@ package logic;
 
 public class Address {
 
-	private String street;
-
-	public String getStreet() {
-		return street;
+	private String streetname;
+	private int houseNumber;
+	private String city;
+	public Address(String streetname, int houseNumber, String city) {
+		super();
+		this.streetname = streetname;
+		this.houseNumber = houseNumber;
+		this.city = city;
+	}
+	public String getStreetname() {
+		return streetname;
+	}
+	public void setStreetname(String streetname) {
+		this.streetname = streetname;
+	}
+	public int getHouseNumber() {
+		return houseNumber;
+	}
+	public void setHouseNumber(int houseNumber) {
+		this.houseNumber = houseNumber;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
 	}
 
-	public void setStreet(String street) {
-		this.street = street;
-	}
+
 }
