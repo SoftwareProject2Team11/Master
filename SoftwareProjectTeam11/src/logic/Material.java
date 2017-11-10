cf1af4becf6ca23e6c0b4c4ca8a4ec7147a50c9b
tomsaccasyn <tom.saccasyn@student.ehb.be>
2017-11-10 15:22:27 +0100

    Revert "Update Material Klasse"
    This reverts commit 9d0d6f929495f690e0975f1e1b92eeee21e3c4a8.

--- SoftwareProjectTeam11/src/logic/Material.java
+++ SoftwareProjectTeam11/src/logic/Material.java
@@ -2,38 +2,20 @@ package logic;
 
 public class Material {
 	private int materialID;
-	private int trainingID;
 	private String title;
 	private int ISBN;
 	private String author;
-	private String type;
 	
-	
-	public Material(int ISBN,int trainingID,String author,String title,String type)
+	public Material(int ISBN,String author,String title)
 	{
 		this.materialID++;
-		this.setTrainingID(trainingID);
 		this.setTitle(title);
 		this.setAuthor(author);
 		this.setISBN(ISBN);
-		this.setType(type);
-	}
-	public int getTrainingID() {
-		return trainingID;
-	}
-	public void setTrainingID(int trainingID) {
-		this.trainingID = trainingID;
 	}
 	public int getMaterialID()
 	{
 		return this.materialID;
-	}
-
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
 	}
 	public String getTitle()
 	{
