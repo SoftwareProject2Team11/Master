82767a261a2167653eed887b50a9bfb37a9ab74a
Basil LK <32955738+Basil-LK@users.noreply.github.com>
2017-11-12 20:25:20 +0100

    Adding certificat and employee

---
+++ SoftwareProjectTeam11/src/logic/Certificat.java
@@ -0,0 +1,29 @@
+package logic;
+
+public class Certificat {
+	
+	private int certificatId;
+	private String certificatName;
+	private boolean result;
+	
+	public int getCertificatId() {
+		return certificatId;
+	}
+	public void setCertificatId(int certificatId) {
+		this.certificatId = certificatId;
+	}
+	public String getCertificatName() {
+		return certificatName;
+	}
+	public void setCertificatName(String certificatName) {
+		this.certificatName = certificatName;
+	}
+	public boolean isResult() {
+		return result;
+	}
+	public void setResult(boolean result) {
+		this.result = result;
+	}
+
+	
+}
