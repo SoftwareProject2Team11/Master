82078d8b763112526cc0f60f30a6b4c2f3796027
KatrienVM <32959311+KatrienVM@users.noreply.github.com>
2017-11-13 15:53:35 +0100

    progress person DAO

--- SoftwareProjectTeam11/src/logic/PersonDAO.java
+++ SoftwareProjectTeam11/src/logic/PersonDAO.java
@@ -3,22 +3,34 @@
  */
 package logic;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.CriteriaQuery;
 
 /**
  * @author Katrien
  *
  */
-public class PersonDAO {
-	SessionFactory sessionFactory;
-	Session s;
+public class PersonDAO extends Connection{
 	
 	public PersonDAO()
 	{
 		super();
 }
+	public ArrayList<Person> getAll(){
+		ArrayList<Person> persons = new ArrayList<Person>();
+		
+			Person person = (Person)s.get(Person.class);
+			persons.add(person);
+	}
 	/*
 	public ArrayList<Login> getAll()
 	{
