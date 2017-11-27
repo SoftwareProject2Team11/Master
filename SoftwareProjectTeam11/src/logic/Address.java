a44999441a7bdb7eedd55cedb223cddb9dd8f245
oussamaelaazmani <oussama.el.aazmani@student.ehb.be>
2017-11-27 10:13:18 +0100

    AddressDAO aanpassen

--- SoftwareProjectTeam11/src/logic/Address.java
+++ SoftwareProjectTeam11/src/logic/Address.java
@@ -1,5 +1,14 @@
 package logic;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.Session; 
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import javax.persistence.Query;
+
 //@Entity
 //@Table(name="Address")
 public class Address {
@@ -51,4 +60,86 @@ public class Address {
 	public void setAddressId(int AddressId) {
 		this.AddressId = AddressId;
 	}
+	
+	public boolean addAddress(Address address)
+	{
+	//opening session
+	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+	Session session = sessionFactory.openSession();
+	session.beginTransaction();
+	
+	
+	session.save(address);
+		
+		
+	//closing session
+	session.getTransaction().commit();
+	System.out.println("Statement Worked!");
+	session.close();
+	sessionFactory.close();
+		
+	}
+	
+	
+	public List<Address> getAllAddress()
+	{
+			//opening session
+	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+	Session session = sessionFactory.openSession();
+	session.beginTransaction();
+	
+	
+	Query query = session.createQuery("FROM address");
+	List<Address> lijst = query.list();
+		
+		
+	//closing session
+	session.getTransaction().commit();
+	System.out.println("Statement Worked!");
+	session.close();
+	sessionFactory.close();
+	
+	return lijst;
+	}
+	
+	public Address getAddressById(int id)
+	{
+			//opening session
+	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+	Session session = sessionFactory.openSession();
+	session.beginTransaction();
+	Address a = new Address();
+	
+	a = (Address)session.get(Address.class,id);
+
+		
+		
+	//closing session
+	session.getTransaction().commit();
+	System.out.println("Statement Worked!");
+	session.close();
+	sessionFactory.close();
+	
+	return a;
+	}
+	
+	public Address getAddressByStreet(String name)
+	{
+			//opening session
+	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+	Session session = sessionFactory.openSession();
+	session.beginTransaction();
+
+	Query query = session.createQuery("FROM address WHERE streetname Like '%"+name+"%'");
+	Address a = (Address)query.uniqueResult();
+		
+		
+	//closing session
+	session.getTransaction().commit();
+	System.out.println("Statement Worked!");
+	session.close();
+	sessionFactory.close();
+	return a;
+	}
+	
 }
