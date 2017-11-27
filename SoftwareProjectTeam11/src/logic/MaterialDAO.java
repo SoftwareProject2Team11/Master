cc2e7d8edb4485f9d94c4112c806477453278afd
tomsaccasyn <tom.saccasyn@student.ehb.be>
2017-11-27 12:19:14 +0100

    LoginDAO en MaterialDAO aangepast zodat ze zonder memory leaks zijn

--- SoftwareProjectTeam11/src/logic/MaterialDAO.java
+++ SoftwareProjectTeam11/src/logic/MaterialDAO.java
@@ -17,76 +17,152 @@ public class MaterialDAO {
 
 	
 	
-		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
-		Session s = sessionFactory.openSession();
-		public MaterialDAO()
-		{
-		s.beginTransaction();	
-		}
+		
 	public ArrayList<Material> getAll()
 	{
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session s = sessionFactory.openSession();
+		
+		s.beginTransaction();
+		
 		ArrayList<Material>Materials = new ArrayList<Material>();
 		Material m = new Material();
 		m = (Material)s.get(Material.class,s);
 		Materials.add(m);
-		return Materials;
+
+		   s.getTransaction().commit();
+		   s.close();
+		   sessionFactory.close();
+		
+		   return Materials;
 	}
 	public Material getItemById(int id)
 	{
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session s = sessionFactory.openSession();
+		
+		s.beginTransaction();
+		
 		Material m = new Material();
 		m = (Material)s.get(Material.class,id);
-		return m;
+
+		   s.getTransaction().commit();
+		   s.close();
+		   sessionFactory.close();
+		
+		   return m;
 	}
 	public ArrayList<Material> getItemBytitle(String title)
 	{
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session s = sessionFactory.openSession();
+		
+		s.beginTransaction();
+		
 		ArrayList<Material>Materials = new ArrayList<Material>();
 		Material m = new Material();
 		m = (Material)s.get(Material.class,title);
 		Materials.add(m);
-		return Materials;
-	}
-	public Material getItemByISBN(int ISBN)
-	{
-		Material m = new Material();
-		m = (Material)s.get(Material.class,ISBN);
-		return m;
-	}
-	public ArrayList<Material> getMaterialByAuthor(String author)
-	{
-		ArrayList<Material>MaterialsA = new ArrayList<Material>();
-		Material m = new Material();
-		m = (Material)s.get(Material.class,author);
-		MaterialsA.add(m);
-		return MaterialsA;
-	}
-	public void addMaterial(String title,int ISBN, String author)
-	{
-		Material m = new Material();
-		m.setMaterial(title, ISBN, author);
-		s.save(m);
-	}
-	public void updateTitle(int id,String title)
-	{
-		Material m = (Material) s.load(Material.class, id);
-		m.setTitle(title);
-		
-	}
-	public void updateISBN(int id,int ISBN)
-	{
-		Material m = (Material) s.load(Material.class, id);
-		m.setISBN(ISBN);
-		
-	}
-	public void updateauthor(int id,String author)
-	{
-		Material m = (Material) s.load(Material.class, id);
-		m.setAuthor(author);
-		
-	}
-		
-	public void finalize() {
+
 		   s.getTransaction().commit();
 		   s.close();
 		   sessionFactory.close();
-		  }
+		
+		   return Materials;
+	}
+	public Material getItemByISBN(int ISBN)
+	{
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session s = sessionFactory.openSession();
+		
+		s.beginTransaction();
+		
+		Material m = new Material();
+		m = (Material)s.get(Material.class,ISBN);
+
+		   s.getTransaction().commit();
+		   s.close();
+		   sessionFactory.close();
+		
+		   return m;
+	}
+	public ArrayList<Material> getMaterialByAuthor(String author)
+	{
+		
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session s = sessionFactory.openSession();
+		
+		s.beginTransaction();
+		
+		ArrayList<Material>MaterialsA = new ArrayList<Material>();
+		
+		Material m = new Material();
+		m = (Material)s.get(Material.class,author);
+		MaterialsA.add(m);
+
+		   s.getTransaction().commit();
+		   s.close();
+		   sessionFactory.close();
+		
+		   return MaterialsA;
+		
+	}
+	public void addMaterial(String title,int ISBN, String author)
+	{
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session s = sessionFactory.openSession();
+		
+		s.beginTransaction();
+		
+		Material m = new Material();
+		m.setMaterial(title, ISBN, author);
+		s.save(m);
+
+		   s.getTransaction().commit();
+		   s.close();
+		   sessionFactory.close();
+	}
+	public void updateTitle(int id,String title)
+	{
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session s = sessionFactory.openSession();
+		
+		s.beginTransaction();
+		
+		Material m = (Material) s.load(Material.class, id);
+		m.setTitle(title);
+
+		   s.getTransaction().commit();
+		   s.close();
+		   sessionFactory.close();
+	}
+	public void updateISBN(int id,int ISBN)
+	{
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session s = sessionFactory.openSession();
+		
+		s.beginTransaction();
+		
+		Material m = (Material) s.load(Material.class, id);
+		m.setISBN(ISBN);
+
+		   s.getTransaction().commit();
+		   s.close();
+		   sessionFactory.close();
+	}
+	public void updateauthor(int id,String author)
+	{
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session s = sessionFactory.openSession();
+		
+		s.beginTransaction();
+		
+		Material m = (Material) s.load(Material.class, id);
+		m.setAuthor(author);
+
+		   s.getTransaction().commit();
+		   s.close();
+		   sessionFactory.close();
+	}
+		
 }
